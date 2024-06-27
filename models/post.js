fd9e157c83c18783f397b9697c754fdc5e1d5260
pavlo <ramison213@gmail.com>
2024-06-27 16:27:00 +0300

    added models user, post, comment

---
+++ models/post.js
@@ -0,0 +1,10 @@
+const {Schema, model} = require('mongoose');
+
+const postSchema = new Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+}, { timestamps: true });
+
+module.exports = model('Post', postSchema);
