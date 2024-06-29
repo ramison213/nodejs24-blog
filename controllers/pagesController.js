fbb087750de13a61d8afd31c9999292313682ff0
pavlo <ramison213@gmail.com>
2024-06-29 15:38:37 +0300

    added renderPage method

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -1,4 +1,22 @@
 const logger = require('../utils/logger')('pages controller');
 
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} resp
+ * @param {import('express').NextFunction} next
+ */
 
-module.exports = {}
+
+/**
+ * @param {string} templateName
+ */
+
+function renderPage(templateName) {
+    return (req, resp) => {
+        resp.render(templateName, req.__pageContext);
+    }
+}
+
+module.exports = {
+    renderPage
+}
