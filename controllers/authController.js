ebca4a0f86bc710f097fd4fb6ec8190cb7994a66
pavlo <ramison213@gmail.com>
2024-06-29 15:48:22 +0300

    added createUserAccount

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,43 @@
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const { ROLES } = require('../middlewares/authContext');
+const logger = require('../utils/logger')('auth controller');
+const { AuthError } = require('../errors');
 
+const MESSAGES = {
+    TAKEN: 'Cannot use this username',
+    AUTH_UNKNOWN_ERROR: 'Unknown auth error'
+}
 
-module.exports = {}
+async function createUserAccount(req, resp, next) {
+    try {
+        const { username, password } = req.body;
+        const salt = await bcrypt.genSalt(7);
+        const hashedPass = await bcrypt.hash(password, salt);
+        const role = ROLES.user;
+
+        const newUser = new User({ username, password: hashedPass, role });
+        await newUser.save();
+
+        req.__authContext = { username, role };
+        logger.info(`User [${newUser.username}] with role [${newUser.role}] successfully created`);
+
+        next();
+    } catch (err) {
+        if (err.name === 'MongoServerError' && err.code === 11000) {
+            return next(new AuthError({
+                msg: MESSAGES.TAKEN,
+                errors: { username: [MESSAGES.TAKEN] }
+            }));
+        }
+
+        next(new AuthError({
+            msg: MESSAGES.AUTH_UNKNOWN_ERROR,
+            errors: { username: [MESSAGES.AUTH_UNKNOWN_ERROR] }
+        }));
+    }
+}
+
+module.exports = {
+    createUserAccount
+}
