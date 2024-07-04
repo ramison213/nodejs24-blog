39aa134f5d8208ee8edacc7ae397590121471edf
pavlo <ramison213@gmail.com>
2024-07-04 15:48:56 +0300

    added model Comment, refactored model Post

--- models/comment.js
+++ models/comment.js
@@ -2,8 +2,8 @@ const {Schema, model} = require('mongoose');
 
 const commentSchema = new Schema({
     content: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
