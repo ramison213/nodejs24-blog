a5ee97baaddb8a14bebdcc879fc24d5caf8201e0
pavlo <ramison213@gmail.com>
2024-07-04 16:03:24 +0300

    added jsdoc to user_service

--- services/user_service.js
+++ services/user_service.js
@@ -1,17 +1,28 @@
 const User = require('../models/user');
 
+/**
+ * @param {string} username
+ * @returns {Promise<object | null>}
+ */
+
 function findByUserName(username) {
     return User.findOne({ username }, 'password', { lean: true });
 }
 
+/**
+ * @param {string} username
+ * @param {string} hashedPass
+ * @param {string} role
+ * @returns {Promise<object>}
+ */
+
 async function saveNewUser({ username, hashedPass, role }) {
     const newUser = new User({ username, password: hashedPass, role });
     await newUser.save();
-
     return newUser;
 }
 
 module.exports = {
     findByUserName,
     saveNewUser
-}
+};
