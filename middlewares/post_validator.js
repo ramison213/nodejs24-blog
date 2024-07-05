eb090d3d9b87adb2d3448ac37bd125e1b299176f
pavlo <ramison213@gmail.com>
2024-07-05 10:55:52 +0300

    added post validator file

--- middlewares/post_validator.js
+++ middlewares/post_validator.js
@@ -7,36 +7,35 @@ const MESSAGES = {
     tooShort: (min) => `Should be at least ${min} characters`
 };
 
-const userDataSchema = yup.object({
-    username: yup.string()
+const postSchema = yup.object({
+    postTitle: yup.string()
         .typeError(MESSAGES.wrongType('a string'))
         .required(MESSAGES.missing)
-        .min(4, MESSAGES.tooShort(4)),
-    password: yup.string()
+        .min(6, MESSAGES.tooShort(6)),
+    postContent: yup.string()
         .typeError(MESSAGES.wrongType('a string'))
         .required(MESSAGES.missing)
-        .min(8, MESSAGES.tooShort(8))
+        .min(10, MESSAGES.tooShort(10))
 });
 
-const userValidator = async (req, resp, next) => {
+const postValidator = async (req, resp, next) => {
     try {
-        await userDataSchema.validate(req.body, { abortEarly: false });
+        await postSchema.validate(req.body, { abortEarly: false });
         next();
     } catch (err) {
         const errors = err.inner.reduce((acc, curr) => {
             if (!acc[curr.path]) {
                 acc[curr.path] = [];
             }
-
             acc[curr.path].push(curr.message);
 
             return acc;
         }, {});
 
-        next(new ValidationError({ msg: 'Invalid user credentials format', errors }));
+        next(new ValidationError({ msg: 'Invalid post data format', errors }));
     }
 }
 
 module.exports = {
-    userValidator
-}
+    postValidator
+};
