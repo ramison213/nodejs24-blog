32ab6a0ae016fe8002e3276c8cda5a27325507e9
pavlo <ramison213@gmail.com>
2024-07-06 13:48:45 +0300

    created post_service and postController.js

---
+++ controllers/postController.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { findByUserName } = require('../services/user_service');
+const { saveNewPost, getAllPosts } = require('../services/post_service');
+const logger = require('../utils/logger')(path.basename(__filename));
+
+const withAsyncHandler = (fn) => async (req, resp, next) => {
+    try {
+        await fn(req, resp, next);
+    } catch (err) {
+        logger.error(err);
+        next(err);
+    }
+}
+
+async function createPost(req, resp, next) {
+    const { postTitle, postContent } = req.body;
+    const username = req.session.context.username;
+
+    try {
+        const author = await findByUserName(username);
+        const newPost = await saveNewPost({ title: postTitle, content: postContent, author: author._id });
+
+        logger.info(`new post [${newPost.title}] successfully created by [${newPost.author}]`);
+
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = {
+    createPost: withAsyncHandler(createPost)
+
+}
---
+++ services/post_service.js
@@ -0,0 +1,87 @@
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+const moment = require('moment');
+
+/**
+ * @typedef {Object} Author
+ * @property {string} _id
+ * @property {string} username
+ */
+
+/**
+ * @typedef {Object} Comment
+ * @property {string} _id
+ * @property {string} content
+ * @property {Author} author
+ * @property {string} createdAt
+ * @property {string} updatedAt
+ */
+
+/**
+ * @typedef {Object} Post
+ * @property {string} _id
+ * @property {string} title
+ * @property {string} content
+ * @property {Author} author
+ * @property {string} createdAt
+ * @property {string} updatedAt
+ * @property {Comment[]} comments
+ */
+
+/**
+ * @returns {Promise<Post[]>}
+ */
+async function getAllPosts() {
+    return Post
+        .find()
+        .populate('author', 'username')
+        .populate('comments')
+        .sort({ createdAt: -1 })
+        .lean();
+}
+
+/**
+ * Formats the date
+ * @param {Post[]} posts
+ * @returns {Post[]} formatted posts
+ */
+function formatPostDates(posts) {
+    return posts
+        .map(post => ({
+            ...post,
+            createdAt: moment(post.createdAt).format('DD.MM.YYYY HH:mm')
+        }));
+}
+
+/**
+ * @param {String} title
+ * @param {String} content
+ * @param {String} author
+ * @returns {Promise<Object>}
+ */
+async function saveNewPost({ title, content, author }) {
+    const newPost = new Post({ title, content, author });
+    await newPost.save();
+
+    return newPost;
+}
+
+/**
+ * @param {String} postId
+ * @returns {Promise<Object>}
+ */
+async function deletePostById(postId) {
+    const post = await Post.findByIdAndDelete(postId);
+    if (post) {
+        await Comment.deleteMany({ post: postId });
+    }
+
+    return post;
+}
+
+module.exports = {
+    getAllPosts,
+    saveNewPost,
+    deletePostById,
+    formatPostDates
+};
