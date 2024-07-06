270582ab3951ed048305f82b56eee2d99b6f0fb2
pavlo <ramison213@gmail.com>
2024-07-06 13:49:20 +0300

    added fetchAllPosts to pagesController

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { getAllPosts, formatPostDates } = require('../services/post_service');
 const logger = require('../utils/logger')(path.basename(__filename));
 
 /**
@@ -6,7 +7,6 @@ const logger = require('../utils/logger')(path.basename(__filename));
  * @param {import('express').Response} resp
  * @param {import('express').NextFunction} next
  */
-
 function addPageContext(req, resp, next) {
     const isLoggedIn = !!req.session?.context?.role;
     req.__pageContext = {
@@ -22,9 +22,22 @@ function addPageContext(req, resp, next) {
 }
 
 /**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} resp
+ * @param {import('express').NextFunction} next
+ */
+async function fetchAllPosts(req, _resp, next) {
+    const postsList = await getAllPosts();
+    const formatedPostsList = formatPostDates(postsList);
+
+    req.__pageContext.postsList = formatedPostsList;
+
+    next();
+}
+
+/**
  * @param {string} templateName
  */
-
 function renderPage(templateName) {
     return (req, resp) => {
         resp.render(templateName, req.__pageContext);
@@ -33,5 +46,6 @@ function renderPage(templateName) {
 
 module.exports = {
     addPageContext,
+    fetchAllPosts,
     renderPage
 }
